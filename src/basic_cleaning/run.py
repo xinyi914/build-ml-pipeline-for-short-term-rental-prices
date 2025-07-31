d5e0a2cc341a57a8571f1e9d350b4aabfdca8a09
Xinyi Mao <xinyimao914@gmail.com>
2025-07-30 23:51:35 -0400

    fix the problem that has longitude outlier in the cleaning dataset

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,8 @@ def go(args):
 
     # logger.info("Dropping null values")
     # df = df.dropna()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info("Saving cleaned dataset to W&B")
     df.to_csv("clean_sample.csv",index=False)
